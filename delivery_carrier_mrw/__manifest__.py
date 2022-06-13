8a5401445aee85b990638991a1253253488f509c
cubells <vicent@vcubells.net>
2022-06-13 11:11:02 +0200

    First version of module

---
+++ delivery_carrier_mrw/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Delivery Carrier: MRW',
+    'version': '1.0',
+    'category': 'Generic Modules/Warehouse',
+    'author': "Punt Sistemes, S.L.U",
+    'website': "https://odoo-community.org/",
+    'depends': [
+        'stock_picking_delivery_info_computation',
+    ],
+    'data': [
+        'data/mrw_data.xml',
+        'data/mrw_templates.xml',
+        'views/delivery_carrier_view.xml',
+        ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
